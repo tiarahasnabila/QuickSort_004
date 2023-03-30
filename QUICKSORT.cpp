bd57dcf6c2c9d66cd0c7514e2426d5a6774e8b46
tiarahasnabila <tiarahasnabila@gmail.com>
2023-03-30 11:57:25 +0700

    prosedur swaps

--- QUICKSORT.cpp
+++ QUICKSORT.cpp
@@ -29,3 +29,97 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)									// Langkah 1
+		return;
+
+	//partition the list into two parts;
+	//one containing elements less that or equal to pivot
+	//outher containing elements greather than pivot
+
+	pivot = arr[low];								// Langkah 2
+
+	i = low + 1;									// Langkah 3
+	j = high;										// Langkah 4
+
+
+	while (i <= j)									// Langkah 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))	// Langkah 5
+		{
+			i++;									// Langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))		// Langkah 7
+		{
+			j--;									// Langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+													// Langkah 9
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j)									// Langkah 11
+	{
+		// move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	q_short(low, j - 1);							// Langkah 12
+
+	//sort the list on the right of pivot using quick sort 
+	q_short(j + 1, high);							// Langkah 13
+
+
+}
+
+
+void display() {
+	cout << "\n-----------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "\n-----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "";
+	}
+
+	cout << "\n\nNumber of comparasions : " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
+
+
+int main()
+{
+
+	input();
+	//sort the array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
