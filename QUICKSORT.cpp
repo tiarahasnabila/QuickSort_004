ef662e3ec18eb367cadb67cd7ea63053c3852294
tiarahasnabila <tiarahasnabila@gmail.com>
2023-03-30 12:00:34 +0700

    program utama

--- QUICKSORT.cpp
+++ QUICKSORT.cpp
@@ -111,3 +111,15 @@ void display() {
 	cout << "Number of data movements: " << mov_count << endl;
 }
 
+
+int main()
+{
+
+	input();
+	//sort the array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
