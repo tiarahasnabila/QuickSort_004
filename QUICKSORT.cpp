69b18d7c619d17cbd3a402541c1beb37e6c61393
tiarahasnabila <tiarahasnabila@gmail.com>
2023-03-30 10:45:25 +0700

    mendeklarasikan variabel

--- QUICKSORT.cpp
+++ QUICKSORT.cpp
@@ -2,4 +2,7 @@
 using namespace std;
 
 //array of integers to hold values
-int arr
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
+int n;
